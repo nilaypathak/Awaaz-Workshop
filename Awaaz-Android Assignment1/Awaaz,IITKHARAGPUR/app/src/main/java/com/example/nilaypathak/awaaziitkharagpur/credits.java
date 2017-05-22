b4549a80be283975d67be9d0c0ec2f16dd476e3e
nilaypathak <nilay.pathak.iitkgp@gmail.com>
2017-05-23 01:46:24 +0530

    upload

---
+++ Awaaz-Android Assignment1/Awaaz,IITKHARAGPUR/app/src/main/java/com/example/nilaypathak/awaaziitkharagpur/credits.java
@@ -0,0 +1,30 @@
+package com.example.nilaypathak.awaaziitkharagpur;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Nilay Pathak on 5/22/2017.
+ */
+public class credits extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.credits, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Credits");
+    }
+}
